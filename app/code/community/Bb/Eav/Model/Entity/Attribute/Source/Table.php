a0a9527cff99f3a331f72868fe841ecef2dac73c
George BABARUS <george.babarus@zitec.ro>
2014-11-04 19:47:06 +0200

    removed unused code

--- app/code/community/Bb/Eav/Model/Entity/Attribute/Source/Table.php
+++ app/code/community/Bb/Eav/Model/Entity/Attribute/Source/Table.php
@@ -39,38 +39,6 @@
 class Bb_Eav_Model_Entity_Attribute_Source_Table extends Mage_Eav_Model_Entity_Attribute_Source_Table
 {
 
-    /**
-     * Retrieve Full Option values array
-     *
-     * @param bool $withEmpty       Add empty option to array
-     * @param bool $defaultValues
-     * @return array
-     */
-    public function getAllOptions($withEmpty = true, $defaultValues = false)
-    {
-        $storeId = $this->getAttribute()->getStoreId();
-        if (!is_array($this->_options)) {
-            $this->_options = array();
-        }
-        if (!is_array($this->_optionsDefault)) {
-            $this->_optionsDefault = array();
-        }
-        if (!isset($this->_options[$storeId])) {
-            $collection = Mage::getResourceModel('eav/entity_attribute_option_collection')
-                ->setPositionOrder('asc')
-                ->setAttributeFilter($this->getAttribute()->getId())
-                ->setStoreFilter($this->getAttribute()->getStoreId())
-                ->load();
-            $this->_options[$storeId]        = $collection->toOptionArray();
-            $this->_optionsDefault[$storeId] = $collection->toOptionArray('default_value');
-        }
-        $options = ($defaultValues ? $this->_optionsDefault[$storeId] : $this->_options[$storeId]);
-        if ($withEmpty) {
-            array_unshift($options, array('label' => '', 'value' => ''));
-        }
-
-        return $options;
-    }
 
     /**
      * return an array of selected option label and value of attribute for product
